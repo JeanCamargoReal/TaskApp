4a53c753e2b04e201efa4c2373ff8228a2faf57b
JeanCamargoReal <jeancamargoreal@icloud.com>
2025-03-26 09:41:40 -0300

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by Jean Camargo on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
